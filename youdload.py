9823e1ec5468c2bf5f2296086121b1eba3af73f4
youssefelmiany1000 <76072513+youssefelmiany1000@users.noreply.github.com>
2020-12-23 19:13:42 -0500

    Update youdload.py

--- youdload.py
+++ youdload.py
@@ -1,7 +1,3 @@
-y("hello world")
-print(y)
-t("Youdload tool is an Arabic tool for downloading videos from YouTube created by the young engineer 'Youssef Al-Mayani'")
-print(t)
 from pafy import new
 url = input('enter your link here: ')
 video = new(url)
